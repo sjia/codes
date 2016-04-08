cd3fe50a548588a855c13fe7a3d8b59c12d933ca
sjia <enluan.jia@outlook.com>
2016-04-08 17:05:02 +0800

    new

---
+++ MyWebSite/prefs.js
@@ -0,0 +1,10 @@
+//??????
+user_pref("app.update.auto", false);
+user_pref("app.update.enabled", false);//?????????
+user_pref("app.update.migrated.updateDir", false);
+user_pref("app.update.lastUpdateTime.browser-cleanup-thumbnails", 0);
+user_pref("app.update.lastUpdateTime.datareporting-healthreport-lastDailyCollection", 0);
+user_pref("app.update.disable_button.showUpdateHistory", false);
+user_pref("app.update.service.enabled", false);
+user_pref("browser.search.update", false); //?????????,????
+user_pref("extensions.update.enabled", false); //???????????
